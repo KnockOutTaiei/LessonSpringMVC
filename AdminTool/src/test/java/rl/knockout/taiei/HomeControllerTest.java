798957a44ca91b51fdcbbd8fcc9929da8672a8ce
KnockOutTaiei <67018206+KnockOutTaiei@users.noreply.github.com>
2020-06-29 14:44:26 +0900

    WIP: Add test code
    add goMenu()
    add historySearch()
    * When and where BindingResult,Model,.... are initialized and How to initialize them

--- AdminTool/src/test/java/rl/knockout/taiei/HomeControllerTest.java
+++ AdminTool/src/test/java/rl/knockout/taiei/HomeControllerTest.java
@@ -1,8 +1,15 @@
 package rl.knockout.taiei;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.DataBinder;
+import org.springframework.web.servlet.ModelAndView;
 
 import rl.knockout.taiei.contoller.*;
 import rl.knockout.taiei.model.*;
@@ -15,7 +22,43 @@ public class HomeControllerTest {
 
     @Test
     public void goHome() {
-        assertEquals("home", homeController.home(null, null));
+    	User user= new User();
+    	//Model model = new Model();
+        assertEquals("home", homeController.home(user, null));
+    }
+    
+    @Test
+    public void goMenu() {
+    	User user= new User();
+    	user.setPrivilegeLogin(false);
+    	
+        assertEquals("login", homeController.menu(user));
+    }
+    
+    @Before
+    public void setupBindingResult() {
+    	//TODO:
+    }
+    
+    @Test
+    public void historySearchTest() {//BindingResultがどこで初期化されているのかわからず、したがってテスト時にどう初期化すればよいのか
+    	//Arrange
+    	HistorySearchForm historySearchForm = homeController.setupHistorySearchForm();
+    	historySearchForm.setAcc_id("temp");
+    	historySearchForm.setLogin_name("Test Taste");
+    	historySearchForm.setOrder_status("tetetetest");
+    	historySearchForm.setDateKindString("confirm_date");
+    	historySearchForm.setBeginDate("2020-09-07");
+    	historySearchForm.setEndDate("1111-01-01");
+    	historySearchForm.setNowPage(9);
+    	DataBinder dataBinder = new DataBinder(historySearchForm);
+    	ModelAndView modelAndView = new ModelAndView("historySearch");
+    	
+    	//Act
+    	ModelAndView actalResult = homeController.historySearch(historySearchForm, dataBinder.getBindingResult());
+    	
+    	//Assert
+        assertThat(actalResult, equalTo(modelAndView));
     }
 
 }
