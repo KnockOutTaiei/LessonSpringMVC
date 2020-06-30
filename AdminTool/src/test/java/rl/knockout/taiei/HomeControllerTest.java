74ad2ffc3ba8aa2360c2f4c04a9eee55b7df1df3
KnockOutTaiei <67018206+KnockOutTaiei@users.noreply.github.com>
2020-06-30 17:51:29 +0900

    WIP: 400 Error
    Using mock(MockMvc) is succeeded

--- AdminTool/src/test/java/rl/knockout/taiei/HomeControllerTest.java
+++ AdminTool/src/test/java/rl/knockout/taiei/HomeControllerTest.java
@@ -1,11 +1,16 @@
 package rl.knockout.taiei;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
@@ -16,10 +21,35 @@ import rl.knockout.taiei.model.*;
 import rl.knockout.taiei.model.dao.*;
 
 import java.sql.*;
+import java.util.ArrayList;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml")
+@WebAppConfiguration
+//https://blog.kakakikikeke.com/2015/07/spring-test-mockmvc.html
 public class HomeControllerTest {
 
     private final HomeController homeController = new HomeController();
-
+    
+    //mockMvcというspringMVCの動作を再現するものを使う。なにこれ。
+    //モック：テストコード入りスタブ
+    //MockMvcは、そのうえ疑似的にリクエストを送るらしい
+    //https://terasolunaorg.github.io/guideline/5.4.1.RELEASE/ja/UnitTest/ImplementsOfUnitTest/UsageOfLibraryForTest.html#mockmvc
+    private MockMvc mockMvc;
+    @Before
+    public void setup() {
+      mockMvc = MockMvcBuilders.standaloneSetup(homeController).alwaysDo(log()).build();
+    }
+    
     @Test
     public void goHome() {
     	User user= new User();
@@ -32,7 +62,7 @@ public class HomeControllerTest {
     	User user= new User();
     	user.setPrivilegeLogin(false);
     	
-        assertEquals("login", homeController.menu(user));
+        assertThat(homeController.menu(user),equalTo("redirect:login"));
     }
     
     @Before
@@ -41,8 +71,9 @@ public class HomeControllerTest {
     }
     
     @Test
-    public void historySearchTest() {//BindingResultがどこで初期化されているのかわからず、したがってテスト時にどう初期化すればよいのか
+    public void historySearchTest() throws Exception {//BindingResultがどこで初期化されているのかわからず、したがってテスト時にどう初期化すればよいのか
     	//Arrange
+    	////stub
     	HistorySearchForm historySearchForm = homeController.setupHistorySearchForm();
     	historySearchForm.setAcc_id("temp");
     	historySearchForm.setLogin_name("Test Taste");
@@ -50,15 +81,32 @@ public class HomeControllerTest {
     	historySearchForm.setDateKindString("confirm_date");
     	historySearchForm.setBeginDate("2020-09-07");
     	historySearchForm.setEndDate("1111-01-01");
-    	historySearchForm.setNowPage(9);
-    	DataBinder dataBinder = new DataBinder(historySearchForm);
-    	ModelAndView modelAndView = new ModelAndView("historySearch");
+    	historySearchForm.setNowPage(1);
+    	
+    	ModelAndView modelAndView = new ModelAndView("home");
+    	
+    	BindingResult bindingResult = new DataBinder(historySearchForm).getBindingResult();
     	
     	//Act
-    	ModelAndView actalResult = homeController.historySearch(historySearchForm, dataBinder.getBindingResult());
-    	
-    	//Assert
-        assertThat(actalResult, equalTo(modelAndView));
-    }
+    	MvcResult result = mockMvc.perform(post("/historySearch").flashAttr("historySearchForm", historySearchForm).flashAttr("result", bindingResult)).andExpect(status().isOk()).andExpect(view().name("historySearch")).andReturn();//https://terasolunaorg.github.io/guideline/5.4.1.RELEASE/ja/UnitTest/ImplementsOfUnitTest/UsageOfLibraryForTest.html#mockmvc
+    	ModelAndView actualResultModelAndView = result.getModelAndView();
 
+    	//Assert
+        assertThat(actualResultModelAndView.getModel().get("historySearchForm"), equalTo(modelAndView.getModel().get("historySearchForm")));
+    }
 }
+/*
+ * Assert  正しいと確認する・正しいと断言する
+ * Assert that actual result is equal to model モデルと実際の結果が等しいことを確認せよ
+ * 
+ * @Autowired DIコンテナに登録  https://qiita.com/ryo2132/items/ec10116238e1e1f333a1
+ * 単にnewするだけでなく、bean.xmlと同じように登録している？
+ * 
+ * 
+ * 
+ * 
+ * 
+ * http://nocturne-life-restoration.hatenablog.com/entry/2014/03/21/205221
+ * SpringFramework4テストコードはServlet3.0が必要になった。
+ * 公式ドキュメントの読みにくさよ
+ * */
